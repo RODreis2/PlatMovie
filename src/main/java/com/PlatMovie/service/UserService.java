be8c1a80be88cf88b487d8174c89db2d88ee92e1
Kelvyn <kelvyn.kaua9@gmail.com>
2025-08-18 07:11:02 -0300

    Feat: New user table

--- src/main/java/com/PlatMovie/service/UserService.java
+++ src/main/java/com/PlatMovie/service/UserService.java
@@ -6,6 +6,7 @@ import com.PlatMovie.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +14,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     public User save(User user){
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
 }
